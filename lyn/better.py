da81eaab40633679344313555ae1168cc69bd018
Lyn_Headley <lyn.headley@mheducation.com>
2021-03-18 09:18:38 -0400

    add more words to content keepers

--- lyn/better.py
+++ lyn/better.py
@@ -9,11 +9,16 @@ mod.list("my_modifiers", desc="content keepers")
 ctx = Context()
 ctx.lists["user.ck"] = {
     "the trilogy": "Bsx",
+    "my name": "Lyn",
+    "my full name": "Lyn Headley",
+    "apostrophized": "'s",
+    "healthy": "1990Theyearsme!",
     "electronic publication": "ePub",
     "the factory": "ePub factory"
 }
 ctx.lists["user.my_modifiers"] = {
-    "backs": "ctrl"
+    "track": "alt",
+    "truck": "ctrl"
 }
 @mod.capture(rule="{user.my_modifiers}")
 def my_modifiers(m) -> str:
