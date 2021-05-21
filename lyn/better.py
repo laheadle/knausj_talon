b85394de7cb9143db429744690c32fb6dcb06e6c
Lyn_Headley <lyn.headley@mheducation.com>
2021-05-21 11:01:43 -0400

    c

--- lyn/better.py
+++ lyn/better.py
@@ -1,22 +1,33 @@
 #common team phrases
 
+import os, json
 from talon import Context, Module
 
 mod = Module()
 mod.list("ck", desc="content keepers")
 mod.list("my_modifiers", desc="content keepers")
 
-ctx = Context()
-ctx.lists["user.ck"] = {
+secret_dictionary = {}
+
+try:
+    with open(os.path.join(os. getcwd(), "secrets.json")) as json_file:
+        secret_dictionary = json.load(json_file)
+except FileNotFoundError:
+    pass
+
+final_dictionary = {
     "the trilogy": "Bsx",
     "my name": "Lyn",
     "my full name": "Lyn Headley",
     "apostrophized": "'s",
-    "healthy": "1990Theyearsme!",
     "electronic publication": "ePub",
     "the factory": "ePub factory",
     "my email": "laheadle@gmail.com"
 }
+final_dictionary.update(secret_dictionary)
+
+ctx = Context()
+ctx.lists["user.ck"] = final_dictionary
 
 @mod.capture(rule="{user.my_modifiers}")
 def my_modifiers(m) -> str:
